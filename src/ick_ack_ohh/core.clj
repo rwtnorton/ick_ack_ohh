b11c81300a96e6f88f83dcc7406fa1ae3c62e5bd
Richard W. Norton <rwtnorton@gmail.com>
2013-11-28 00:43:39 -0600

    Added docstrings for functions.

--- src/ick_ack_ohh/core.clj
+++ src/ick_ack_ohh/core.clj
@@ -6,15 +6,18 @@
 ;;   :x, :o, or :_
 
 (defn new-board
+  "Returns an empty 3x3 board."
   []
   (vec (for [i (range 3)]
          (vec (repeat 3 :_)))))
 
 (defn mark->string
+  "Returns a string representation for a board mark."
   [m]
   (get {:x "X", :o "O", :_ " "} m "?"))
 
 (defn board->string
+  "Returns string representation of a board."
   [b]
   (let [rows (for [row b]
                (str/join "|" (map mark->string row)))
@@ -25,12 +28,14 @@
             rows)))
 
 (defn mark-at?
+  "Returns true iff position has X or O."
   [board row-index col-index]
   (let [v ((board row-index) col-index)]
     (or (= v :x)
         (= v :o))))
 
 (defn place-mark-at
+  "Returns new board with mark applied at position."
   [board mark row-index col-index]
   (let [row (board row-index)
         row (assoc row col-index mark)]
